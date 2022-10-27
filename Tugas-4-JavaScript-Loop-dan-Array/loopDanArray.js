0a2570111ee8c8b3f3cf15a4e6f74483f2e70666
Taufan Ajie Samudro <taufanaziesamudra@gmail.com>
2022-10-27 22:02:09 +0700

    tugas-4

---
+++ Tugas-4-JavaScript-Loop-dan-Array/loopDanArray.js
@@ -0,0 +1,67 @@
+//! Soal 1 Membuat Looping sederhana
+// for (var angka = 0; angka < 10; angka++) {
+//   console.log(angka);
+// }
+
+//! Soal 2 Membuat Looping dengan conditional angka ganjil
+// for (var i = 1; i <= 9; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+//! Soal 3 Membuat Looping dengan angka genap
+// for (var i = 0; i <= 9; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+//! Soal 4 Mengakses Element array
+// let array1 = [1, 2, 3, 4, 5, 6];
+// console.log(array1[5]);
+
+//! Saol 5 Mengurutkan Element aray
+// let array2 = [5, 2, 4, 1, 3, 5];
+// array2.sort();
+// console.log(array2);
+
+// var array3 = [
+//   "selamat",
+//   "anda",
+//   "melakukan",
+//   "perulangan",
+//   "array",
+//   "dengan",
+//   "for",
+// ];
+
+// for (var i = 0; i < array3.length; i++) {
+//   console.log(array3[i]);
+// }
+
+//! Soal 7 Mengelurkan elemet array dan dengan kondisi
+// let array4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (var i = 0; i < array4.length; i++) {
+//   if (i % 2 == 1) {
+//     console.log(array4[i]);
+//   }
+// }
+
+//! Soal 8 Menggabungkan elemet menjadi string
+// let kalimat = ["saya", "sangat", "senang", "belajar", "javascript"];
+// let name = kalimat.join(" ");
+// console.log(name);
+
+//! Soal 9 Menambahkan elemay array
+// var sayuran = [];
+// sayuran.push(
+//   "Kangkung",
+//   "Bayam",
+//   "Buncis",
+//   "Kubis",
+//   "Timun",
+//   "Seledri",
+//   "Tauge"
+// );
+// console.log(sayuran);
