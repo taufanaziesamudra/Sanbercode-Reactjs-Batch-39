b86aa8ff7b955ff418bbd5bda0e5b6f9d74eac22
Taufan Ajie Samudro <taufanaziesamudra@gmail.com>
2022-11-07 21:05:09 +0700

    push tugas-reactjs

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hi, Saya Taufan</h1>
+      <hr />
+      <p>Saya dari Batch 39 sudah berhasil menginstall ReactJS</p>
+    </div>
+  );
+}
+
+export default App;
